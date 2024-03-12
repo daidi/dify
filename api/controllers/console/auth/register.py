7b70d4758a38bcb5a0fab5279545fc5bd5ca5066
PapacyDai <daidi88@sina.com>
2024-03-12 18:33:32 +0800

    feat: add register api

--- api/controllers/console/auth/register.py
+++ api/controllers/console/auth/register.py
@@ -3,45 +3,47 @@ from flask import current_app, request
 from flask_restful import Resource, reqparse
 
 import services
-from constants.languages import supported_language
+from constants.languages import supported_language, languages
 from controllers.console import api
 from controllers.console.setup import setup_required
+from extensions.ext_database import db
 from libs.helper import email, str_len, timezone
 from libs.password import valid_password
-from services.account_service import AccountService, TenantService
+from services.account_service import AccountService, TenantService, RegisterService
 
 
 class RegisterApi(Resource):
-    """Resource for user login."""
+    """Resource for user register."""
 
     @setup_required
     def post(self):
         """Authenticate user and login."""
         parser = reqparse.RequestParser()
-        parser.add_argument('workspace_id', type=str, required=False, nullable=True, location='json')
         parser.add_argument('email', type=email, required=False, nullable=True, location='json')
         parser.add_argument('name', type=str_len(30), required=True, nullable=False, location='json')
         parser.add_argument('password', type=valid_password, required=True, nullable=False, location='json')
-        parser.add_argument('interface_language', type=supported_language, required=True, nullable=False,
-                            location='json')
-        parser.add_argument('timezone', type=timezone, required=True, nullable=False, location='json')
         args = parser.parse_args()
 
-        # todo: Verify the recaptcha
+        account = RegisterService.register(
+            email=args['email'],
+            name=args['name'],
+            password=args['password'],
+            open_id=None,
+            provider=None
+        )
 
-        try:
-            account = AccountService.authenticate(args['email'], args['password'])
-        except services.errors.account.AccountLoginError:
-            return {'code': 'unauthorized', 'message': 'Invalid email or password'}, 401
+        # Set interface language
+        preferred_lang = request.accept_languages.best_match(languages)
+        if preferred_lang and preferred_lang in languages:
+            interface_language = preferred_lang
+        else:
+            interface_language = languages[0]
+        account.interface_language = interface_language
+        db.session.commit()
 
         TenantService.create_owner_tenant_if_not_exist(account)
 
-        AccountService.update_last_login(account, request)
-
-        # todo: return the user info
-        token = AccountService.get_account_jwt_token(account)
-
-        return {'result': 'success', 'data': token}
+        return {'result': 'success', 'data': {}}
 
 
 api.add_resource(RegisterApi, '/register')
