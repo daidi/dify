3fda69f6097be090c398450c3d06027951a7ed7c
PapacyDai <daidi88@sina.com>
2024-03-19 21:22:08 +0800

    feat: add scenarios api

---
+++ api/migrations/versions/5886a8a98774_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 5886a8a98774
+Revises: 64882f1a798f, a8f9b3c45e4a
+Create Date: 2024-03-19 13:21:49.915434
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5886a8a98774'
+down_revision = ('64882f1a798f', 'a8f9b3c45e4a')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
