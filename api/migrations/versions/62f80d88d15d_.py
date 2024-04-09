5059056bb21aef5de23736b6b7c5fdd944d4e80f
PapacyDai <daidi88@sina.com>
2024-04-09 11:14:15 +0800

    fix: debug

---
+++ api/migrations/versions/62f80d88d15d_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 62f80d88d15d
+Revises: a8fec52a67c4, c3311b089690
+Create Date: 2024-04-09 03:14:08.891092
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '62f80d88d15d'
+down_revision = ('a8fec52a67c4', 'c3311b089690')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
