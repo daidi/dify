c0aead5cfcd6fa140b06c4724897b4df8538b75b
PapacyDai <daidi88@sina.com>
2024-04-14 09:27:50 +0800

    feat: 支持ws

--- api/app.py
+++ api/app.py
@@ -283,4 +283,4 @@ def pool_stat():
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=5001)
-    socketio.run(app, host='0.0.0.0', port=5001)
+    socketio.run(app, host='0.0.0.0', port=5001, path='/console/ws')
