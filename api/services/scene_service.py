be722f75f5fd0ff6003c206d9a0b3828726b53d5
PapacyDai <daidi88@sina.com>
2024-05-10 17:57:31 +0800

    fix: debug

--- api/services/scene_service.py
+++ api/services/scene_service.py
@@ -60,7 +60,6 @@ class SceneService:
     def update_scene(scene_id, data, user):
         filtered_data = {k: v for k, v in data.items() if v is not None or k == 'description'}
         scene = SceneService.get_scene(scene_id)
-        print(scene)
         SceneService.check_scene_permission(scene, user)
 
         filtered_data['updated_by'] = user.id
@@ -88,13 +87,14 @@ class SceneService:
 
     @staticmethod
     def check_scene_permission(scene, user):
+        logging.log(f'Checking permission for user {user.id} to access scene {scene.id}')
         if scene.tenant_id != user.current_tenant_id:
-            logging.debug(
+            logging.log(
                 f'User {user.id} does not have permission to access scene {scene.id}')
             raise NoPermissionError(
-                'You do not have permission to access this scene.')
+                'You do not have permission to access this scene tenant.')
         if scene.permission == 'only_me' and scene.created_by != user.id:
-            logging.debug(
+            logging.log(
                 f'User {user.id} does not have permission to access scene {scene.id}')
             raise NoPermissionError(
                 'You do not have permission to access this scene.')
